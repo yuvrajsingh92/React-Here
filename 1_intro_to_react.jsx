3a1b743753437be326c53132255abc681012bdbb
Yuvraj <ayuvraj929@gmail.com>
2026-09-25 16:23:35 +0530

    Learning React

---
+++ 1_intro_to_react.jsx
@@ -0,0 +1,38 @@
+// ! Why Do We Need React.js?Before React, even a small update required refreshing the entire website. For example, when a new chat message was delivered, you wouldn't see it live unless you reloaded the page. This made the user experience slow and difficult to manage. React solves this by updating only the specific parts of the page that change, enabling smooth, real-time updates.
+
+// ! What Is React?React is an open-source JavaScript library used for building user interfaces (UI). It breaks complex web pages down into small, reusable components (like buttons, navigation bars, or comment sections). This modular approach makes the codebase organized, clean, and much easier to read, understand, and maintain.
+
+// ! What is library and diffrent between framework: Library is used to give a feature to a website
+
+// ? create an html element using js
+
+// const h2 = document.createElement("h2");
+// h2.textContent = "Hello from js"
+// document.body.append(h2)
+
+// ! Import and export: Their are two type of import and export in react. named export and default import export
+
+// import array from "./app.js";
+
+// console.log(array)
+
+// import {username} from './app.js' // ? This is how to import using named export
+
+// console.log(username)
+
+// ! Virtual DOM:The Virtual DOM (Document Object Model) is a lightweight, in-memory copy of the actual webpage structure used by JavaScript frameworks like React to update user interfaces efficiently. It saves us from unnessary reloads
+
+// ! JSX: JavaScript XML, it is combination of html and javascript we can write html in react file
+
+// var h1 = <h1>Hello from react</h1>; // ? Like this
+
+// We can call function in diffrent way
+
+// function Hero() {
+//   console.log("Ayush Singh");
+// }
+
+// Hero(); // ? We can call like this and like this also
+// <Hero />;
+
+
